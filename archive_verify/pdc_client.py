3e5f3b501f8903785218da27da5e2cb443460735
b97pla <pontus.larsson@medsci.uu.se>
2023-02-10 10:14:01 +0100

    allow injection of extra dsmc arguments

--- archive_verify/pdc_client.py
+++ archive_verify/pdc_client.py
@@ -9,7 +9,7 @@ import subprocess
 log = logging.getLogger('archive_verify.workers')
 
 
-class PdcClient():
+class PdcClient:
     """
     Base class representing a PDC client.
     Staging and production environments should instantiate PdcClient (default).
@@ -19,13 +19,15 @@ class PdcClient():
         """
         :param archive_name: The name of the archive we shall download
         :param archive_pdc_path: The path in PDC TSM to the archive that we want to download
-        :param archive_pdc_description: The unique description that was used when uploading the archive to PDC
+        :param archive_pdc_description: The unique description that was used when uploading the
+        archive to PDC
         :param job_id: The current rq worker job id
         :param config: A dict containing the apps configuration
         """
         self.dest_root = config["verify_root_dir"]
         self.dsmc_log_dir = config["dsmc_log_dir"]
         self.whitelisted_warnings = config["whitelisted_warnings"]
+        self.dsmc_extra_args = config.get("dsmc_extra_args", {})
         self.archive_name = archive_name
         self.archive_pdc_path = archive_pdc_path
         self.archive_pdc_description = archive_pdc_description
@@ -35,27 +37,45 @@ class PdcClient():
         """
         :returns The unique path where the archive will be downloaded.
         """
-        return "{}_{}".format(os.path.join(self.dest_root, self.archive_name), self.job_id)
+        return f"{os.path.join(self.dest_root, self.archive_name)}_{self.job_id}"
+
+    def dsmc_args(self):
+        """
+        Fetch a list of arguments that will be passed to the dsmc command line. If there are
+        extra arguments specified in the config, with "dsmc_extra_args", these are included as well.
+        If arguments specified in dsmc_extra_args has the same key as the default arguments, the
+        defaults will be overridden.
+
+        :return: a string with arguments that should be appended to the dsmc command line
+        """
+        key_values = {
+            "subdir": "yes",
+            "description": self.archive_pdc_description
+        }
+        key_values.update(self.dsmc_extra_args)
+        args = [f"-{k}='{v}'" for k, v in key_values.items() if v is not None]
+        args.extend([f"-{k}" for k, v in key_values.items() if v is None])
+        return " ".join(args)
 
     def download(self):
         """
         Downloads the specified archive from PDC to a unique location.
         :returns True if no errors or only whitelisted warnings were encountered, False otherwise
         """
-        log.info("Download_from_pdc started for {}".format(self.archive_pdc_path))
-        cmd = "export DSM_LOG={} && dsmc retr {}/ {}/ -subdir=yes -description='{}'".format(self.dsmc_log_dir,
-                                                                                            self.archive_pdc_path,
-                                                                                            self.dest(),
-                                                                                            self.archive_pdc_description)
+        log.info(f"Download_from_pdc started for {self.archive_pdc_path}")
+        cmd = f"export DSM_LOG={self.dsmc_log_dir} && " \
+              f"dsmc retr {self.archive_pdc_path}/ {self.dest()}/ {self.dsmc_args()}"
+
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
         dsmc_output, _ = p.communicate()
         dsmc_exit_code = p.returncode
 
         if dsmc_exit_code != 0:
-            return PdcClient._parse_dsmc_return_code(dsmc_exit_code, dsmc_output, self.whitelisted_warnings)
+            return PdcClient._parse_dsmc_return_code(
+                dsmc_exit_code, dsmc_output, self.whitelisted_warnings)
 
-        log.info("Download_from_pdc completed successfully for {}".format(self.archive_pdc_path))
+        log.info(f"Download_from_pdc completed successfully for {self.archive_pdc_path}")
         return True
 
     def downloaded_archive_path(self):
@@ -67,8 +87,8 @@ class PdcClient():
     @staticmethod
     def _parse_dsmc_return_code(exit_code, output, whitelist):
         """
-        Parses the dsmc output when we've encountered a non-zero exit code. For some certain exit codes,
-        warnings and errors we still want to return successfully.
+        Parses the dsmc output when we've encountered a non-zero exit code. For some certain exit
+        codes, warnings and errors we still want to return successfully.
 
         :param exit_code: The exit code received from the failing dsmc process
         :param output: The text output from the dsmc process
@@ -98,8 +118,9 @@ class PdcClient():
 
             for warning in warnings:
                 if warning not in whitelist:
-                    log.error("A non-whitelisted DSMC warning was encountered. Reporting it as an error! ('{}')".format(
-                        warning))
+                    log.error(
+                        f"A non-whitelisted DSMC warning was encountered. "
+                        f"Reporting it as an error! ('{warning}')")
                     return False
 
             log.info("Only whitelisted DSMC warnings were encountered. Everything is OK.")
@@ -138,8 +159,10 @@ class MockPdcClient(PdcClient):
 
     def download(self):
         if not self.predownloaded_archive_path:
-            log.error(f"No archive containing the name {self.archive_name} found in {self.dest_root}")
+            log.error(
+                f"No archive containing the name {self.archive_name} found in {self.dest_root}")
             return False
         else:
-            log.info(f"Found pre-downloaded archive at {self.predownloaded_archive_path}")
+            log.info(
+                f"Found pre-downloaded archive at {self.predownloaded_archive_path}")
             return True
